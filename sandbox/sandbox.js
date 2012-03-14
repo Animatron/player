0c314e618ea6006422462c32c5d33c6c763de9a4
Ulric Wilfred <shaman.sir@gmail.com>
2012-03-14 16:30:40 +0200

    fix default code indent

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -15,7 +15,7 @@ var defaultCode = [
   '                  .stroke(\'#f00\', 3))',
   '  .add(',
   '    b(\'red-rect\').rect([115, 90], [60, 60]))',
-  '                 .rotate([0, 10], [0, Math.PI]);'
+  '  .rotate([0, 10], [0, Math.PI]);'
 ].join('\n');
 
 function sandbox(codeElmId, canvasElmId, errorsElmId) {
