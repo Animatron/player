639c1e98c913f4b08a4b61838986e6ce618b95bf
Ulric Wilfred <shaman.sir@gmail.com>
2012-06-19 16:51:00 +0300

    [Fix #31445691] Fix intersection

--- anm.collisions.js
+++ anm.collisions.js
@@ -102,14 +102,14 @@ E.prototype.intersects = function(elm, t) {
     var in_bounds = G.__inBounds;
     var this_b = this.bounds(t);
     var other_b = elm.bounds(t);
-    if (in_bounds(other_b, [this_b[0], this_b[1]])) return true;
-    if (in_bounds(other_b, [this_b[3], this_b[1]])) return true;
-    if (in_bounds(other_b, [this_b[0], this_b[3]])) return true;
-    if (in_bounds(other_b, [this_b[2], this_b[3]])) return true;
-    if (in_bounds(this_b, [other_b[0], other_b[1]])) return true;
-    if (in_bounds(this_b, [other_b[3], other_b[1]])) return true;
-    if (in_bounds(this_b, [other_b[0], other_b[3]])) return true;
-    if (in_bounds(this_b, [other_b[2], other_b[3]])) return true;
+    if (in_bounds(other_b, [this_b[0], this_b[1]])) return true; // x1, y1
+    if (in_bounds(other_b, [this_b[2], this_b[1]])) return true; // x2, y1
+    if (in_bounds(other_b, [this_b[2], this_b[3]])) return true; // x2, y2
+    if (in_bounds(other_b, [this_b[0], this_b[3]])) return true; // x1, y2
+    if (in_bounds(this_b, [other_b[0], other_b[1]])) return true; // x1, y1
+    if (in_bounds(this_b, [other_b[2], other_b[1]])) return true; // x2, y1
+    if (in_bounds(this_b, [other_b[2], other_b[3]])) return true; // x2, y2
+    if (in_bounds(this_b, [other_b[0], other_b[3]])) return true; // x1, y2
     if (opts.pathCheck) throw new Error('Not implemented');
     return false;
 }
