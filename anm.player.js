b940ba3af812169e0e8dddcbf9730a09c6ba249d
Ulric Wilfred <shaman.sir@gmail.com>
2012-07-02 18:52:36 +0300

    [Fix #32052081] Fix deep-cloning to include bands and other important stuff

--- anm.player.js
+++ anm.player.js
@@ -1452,6 +1452,11 @@ Element.prototype.dclone = function() {
     if (src_x.path) trg_x.path = src_x.path.clone();
     //if (src_x.image) trg_x.image = src_x.image.clone();
     if (src_x.text) trg_x.text = src_x.text.clone();
+    trg_x.pos = [].concat(src_x.pos);
+    trg_x.reg = [].concat(src_x.reg);
+    trg_x.lband = [].concat(src_x.lband);
+    trg_x.gband = [].concat(src_x.gband);
+    trg_x.keys = obj_clone(src_x.keys);
     return clone;
 }
 Element.prototype._addChild = function(elm) {
