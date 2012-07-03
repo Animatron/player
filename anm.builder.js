08a69c9e10f4c7fc4d5516c3f34ec17e19fb6ba5
Ulric Wilfred <shaman.sir@gmail.com>
2012-07-03 13:28:23 +0300

    [Fix #32050369] Fixed copied fill/stroke for circles

--- anm.builder.js
+++ anm.builder.js
@@ -133,14 +133,16 @@ Builder.prototype.rect = function(pt, rect) {
 Builder.prototype.circle = function(pt, radius) {
     this.x.pos = pt;
     this.x.reg = [ 0, 0 ];
-    var b = this;
     this.x.__bounds = [ 0, 0, radius*2, radius*2];
-    this.paint(function(ctx) {
-        DU.qDraw(ctx, b.s, b.f,
-                 function() {
-                    ctx.arc(0, 0, radius, 0, Math.PI*2, true);
-                 });
-    });
+    this.paint((function(b) {
+        return function(ctx) {
+            var bi = b(this.$);
+            DU.qDraw(ctx, bi.s, bi.f,
+                     function() {
+                        ctx.arc(0, 0, radius, 0, Math.PI*2, true);
+                     });
+        }
+    })(Builder._$));
     if (modCollisions) this.v.reactAs(
             Builder.arcPath(0/*pt[0]*/,0/*pt[1]*/,radius, 0, 1, 12));
     return this;
