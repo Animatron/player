715bff1b38349d4862f8ef0e280aca683dbce2ed
Ulric Wilfred <shaman.sir@gmail.com>
2012-11-19 14:34:45 +0200

    errors spec (empty)

---
+++ tests/spec/01.player/06.errors.spec.js
@@ -0,0 +1,3 @@
+// TODO: errors, thrown in playing process (from modifiers), should be supressed (?), but passed to onerror handler
+// TODO: errors, thrown in player creating process, should be shown in the console.
+// TODO: errors should be thrown only once
