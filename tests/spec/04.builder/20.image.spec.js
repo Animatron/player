9c3399be1380cd4de9a72e7c90660ba2b95417fb
Nikita Dudnik aka Nek <nikdudnik@gmail.com>
2013-02-25 15:58:11 +0700

    Test cached image callback

--- tests/spec/04.builder/20.image.spec.js
+++ tests/spec/04.builder/20.image.spec.js
@@ -19,5 +19,35 @@ describe("image", function() {
              });
          });
     });
+    describe("caching", function() {
+        it("second call callbacks without creating new Image", function() {
+            var done = false;
+            var oldImage = window.Image;
+            var called = false;
+            window.Image = function(s) {
+                console.log("1");
+                var self = this;
+                setTimeout(function() {self.onload();}, 500);
+            };
+            runs(function() {
+                b().image([0,0], "test.png", function() {done = true;});
+            });
+            waitsFor(function() {return done;}, "The callback should fire", 800);
+            runs(function() {
+                window.Image = function(s) {
+                    console.log("2");
+                    called = true;
+                };
+                done = false;
+                b().image([0,0], "test.png", function() {done = true;});
+            });
+            waitsFor(function() {return done;}, "The callback should fire", 800);
+            runs(function() {
+                expect( done ).toBeTruthy();
+                expect( called ).toBeFalsy();
+                window.Image = oldImage;
+            });
+        });
+    });
 
 });
