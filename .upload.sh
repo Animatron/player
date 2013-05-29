805c5743d075807cf201bab184269cfa7da45529
Ulric Wilfred <shaman.sir@gmail.com>
2013-05-29 16:46:13 +0200

    upload script

---
+++ .upload.sh
@@ -0,0 +1,9 @@
+scp index.html www:player/
+scp index.css www:player/
+scp LICENSE www:player/
+#scp -r examples www:player/
+scp -r doc www:player/
+#scp -r games www:player/
+scp -r sandbox www:player/
+scp -r tests www:player/
+
