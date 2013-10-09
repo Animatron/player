6e64d259abfe826e53d4bf0e5c1b435229901fdb
Ulric Wilfred <shaman.sir@gmail.com>
2013-10-09 18:10:49 +0200

    my fail, comparison is wrong here

--- src/module/audio.js
+++ src/module/audio.js
@@ -129,7 +129,7 @@
             if (me._audio_canPlay && window.chrome) {
               if (element.buffered.length == 1) {
                 var end = element.buffered.end(0);
-                if (element.duration - end < 0.05) {
+                if (element.duration - end > 0.05) {
                   element.volume = 0;
                   element.currentTime = end;
                   element.play();
