7fd4ab847e063134c9d84fbc771da480bcf4d2e9
Dmitry Skavish <skavish@gmail.com>
2013-11-13 20:23:15 -0500

    added names to editor easings

--- src/player.js
+++ src/player.js
@@ -3720,27 +3720,27 @@ __registerSegEasing('BOUT',   [0.175, 0.885, 0.320, 1.275, 1.000, 1.000]); // Ba
 __registerSegEasing('BINOUT', [0.680, -0.550, 0.265, 1.550, 1.000, 1.000]); // Back InOut
 
 Easing.__STD_EASINGS = [
-    function(t) { return C['EF_DEF'](t); },
-    function(t) { return C['EF_IN'](t); },
-    function(t) { return C['EF_OUT'](t); },
-    function(t) { return C['EF_INOUT'](t); },
-    function(t) { return t*t; },    // 4
-    function(t) { return t*(2-t); },// 5
-    function(t) {   // 6
+    function(t) { return C['EF_DEF'](t); }, // Default
+    function(t) { return C['EF_IN'](t); },  // In
+    function(t) { return C['EF_OUT'](t); }, // Out
+    function(t) { return C['EF_INOUT'](t); }, // InOut
+    function(t) { return t*t; },    // 4    In Quad
+    function(t) { return t*(2-t); },// 5    Out Quad
+    function(t) {                   // 6    In/Out Quad
         if (t < 0.5) return 2*t*t;
         else {
             t = (t-0.5)*2;
             return -(t*(t-2)-1)/2;
         }
     },
-    function(t) {   // 7
+    function(t) {                   // 7    In Cubic
         return t*t*t;
     },
-    function(t) {   // 8
+    function(t) {                  // 8     Out Cubic
         t = t-1;
         return t*t*t + 1;
     },
-    function(t) {   // 9
+    function(t) {                  // 9     In/Out Cubic
         if (t < 0.5) {
             t = t*2;
             return t*t*t/2;
@@ -3749,22 +3749,22 @@ Easing.__STD_EASINGS = [
             return (t*t*t+2)/2;
         }
     },
-    function(t) {   // 10
+    function(t) {                  // 10   In Sine
         return 1 - Math.cos(t * (Math.PI/2));
     },
-    function(t) {   // 11
+    function(t) {                 // 11    Out Sine
         return Math.sin(t * (Math.PI/2));
     },
-    function(t) {   // 12
+    function(t) {                 // 12    In/Out Sine
         return -(Math.cos(Math.PI*t) - 1)/2;
     },
-    function(t) {   // 13
+    function(t) {                 // 13   In Expo
         return (t<=0) ? 0 : Math.pow(2, 10 * (t - 1));
     },
-    function(t) {   // 14
+    function(t) {                // 14    Out Expo
         return t>=1 ? 1 : (-Math.pow(2, -10 * t) + 1);
     },
-    function(t) {   // 15
+    function(t) {                // 15    In/Out Expo
         if (t<=0) return 0;
         if (t>=1) return 1;
         if (t < 0.5) return Math.pow(2, 10 * (t*2 - 1))/2;
@@ -3772,34 +3772,34 @@ Easing.__STD_EASINGS = [
             return (-Math.pow(2, -10 * (t-0.5)*2) + 2)/2;
         }
     },
-    function(t) {   // 16
+    function(t) {               // 16    In Circle
         return 1-Math.sqrt(1 - t*t);
     },
-    function(t) {   // 17
+    function(t) {              // 17     Out Circle
         t = t-1;
         return Math.sqrt(1 - t*t);
     },
-    function(t) {   // 18
+    function(t) {              // 18     In/Out Cicrle
         if ((t*=2) < 1) return -(Math.sqrt(1 - t*t) - 1)/2;
         return (Math.sqrt(1 - (t-=2)*t) + 1)/2;
     },
-    function(t) {   // 19
+    function(t) {              // 19    In Back
         var s = 1.70158;
         return t*t*((s+1)*t - s);
     },
-    function(t) {   // 20
+    function(t) {             // 20     Out Back
         var s = 1.70158;
         return ((t-=1)*t*((s+1)*t + s) + 1);
     },
-    function(t) {   // 21
+    function(t) {             // 21     In/Out Back
         var s = 1.70158;
         if ((t*=2) < 1) return (t*t*(((s*=(1.525))+1)*t - s))/2;
         return ((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2)/2;
     },
-    function(t) {   // 22
+    function(t) {             // 22     In Bounce
         return 1 - Easing.__STD_EASINGS[23](1-t);
     },
-    function(t) {   // 23
+    function(t) {              // 23    Out Bounce
         if (t < (1/2.75)) {
             return (7.5625*t*t);
         } else if (t < (2/2.75)) {
@@ -3810,7 +3810,7 @@ Easing.__STD_EASINGS = [
             return (7.5625*(t-=(2.625/2.75))*t + .984375);
         }
     },
-    function(t) {   // 24
+    function(t) {             // 24     In/Out Bounce
         if (t < 0.5) return Easing.__STD_EASINGS[22](t*2) * .5 ;
         return Easing.__STD_EASINGS[23](t*2-1) * .5 + .5;
     }
