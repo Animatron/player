03e07ef9e83cc93380dc736412daeabad2920421
Volodymyr Barna <roberto.te.ua@gmail.com>
2014-07-07 13:03:23 +0300

    [EDIT-4729] fixed. Publishing to html5 in production should refer to json on cdn

--- publish.js
+++ publish.js
@@ -115,7 +115,7 @@ var start = (function () {
         _params_ = _u.injectIfNotPresent(_params_, "r", 1);
     }
 
-    var _snapshotUrl_ = './' + filename + (_params_ || '');
+    var _snapshotUrl_ = amazonDomain + '/' + filename + (_params_ || '');
 
     var temp_v = null;
     if (temp_v = _u.extractVal(_params_, 'v')) {
