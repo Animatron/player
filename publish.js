68d88e0a5ec11140ac8d321fe45e011a05a0af35
Ulric Wilfred <shaman.sir@gmail.com>
2014-09-01 15:51:21 +0200

    fix publish.js syntax errors

--- publish.js
+++ publish.js
@@ -21,20 +21,20 @@ var _u = (function () { /* utils */
 
     return {
 
-        parseQueryString = function( queryString ) {
+        parseQueryString: function( queryString ) {
             var params = {}, queries, temp, i, l;
-         
+
             // Split into key/value pairs
             queries = queryString.split("&");
-         
+
             // Convert the array of strings into an object
             for ( i = 0, l = queries.length; i < l; i++ ) {
                 temp = queries[i].split('=');
                 params[temp[0]] = temp[1];
             }
-         
+
             return params;
-        };
+        },
 
 
         extractVal: function (params, name) {
