49cc953ee2877952854f6594293d84326215f1e5
Ulric Wilfred <shaman.sir@gmail.com>
2014-11-06 20:52:50 +0100

    Log a warning instead of firing an error, if scripting is not supported

--- src/module/scripting.js
+++ src/module/scripting.js
@@ -11,16 +11,17 @@ var Player = anm.Player;
 
 var E = anm.Element;
 
-var is = anm.utils.is;
+var is = anm.utils.is,
+    log = anm.log;
 
 E._customImporters.push(function(source, type, importer, import_id) {
   if (importer === 'ANM') {
 
     switch(type) {
       case 2: // TYPE_SCENE
-        if (source[4] && is.not_empty(source[4])) throw new Error('Scripting is temporarily not supported');
+        if (source[4] && is.not_empty(source[4])) log.warn('Scripting from Animatron Editor is temporarily not supported');
       case 255: // TYPE_LAYER
-        if (source[8] && is.not_empty(source[8])) throw new Error('Scripting is temporarily not supported');
+        if (source[8] && is.not_empty(source[8])) log.warn('Scripting from Animatron Editor is temporarily not supported');
     }
   }
 });
