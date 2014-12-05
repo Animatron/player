37963fb8a790a63c0e09b6c27f17887f34c93642
Ulric Wilfred <shaman.sir@gmail.com>
2014-12-05 20:58:33 +0100

    removed games and sandbox, while they are in progress, include up-to-date API docs

--- .upload.sh
+++ .upload.sh
@@ -1,9 +1,7 @@
 scp index.html www:player
 scp index.css www:player
 scp LICENSE www:player
-#scp -r examples www:player/examples
 scp -r doc www:player/doc
 #scp -r games www:player/games
-scp -r sandbox www:player/sandbox
-scp -r tests www:player/tests
+#scp -r sandbox www:player/sandbox
 
