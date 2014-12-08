9e4d3d5fa08d84b80115ff0cc789e7c26f86ce6d
Ulric Wilfred <shaman.sir@gmail.com>
2014-12-08 18:17:02 +0100

    PlayerManager docs

--- src/anm/player_manager.js
+++ src/anm/player_manager.js
@@ -3,7 +3,17 @@
 var events = require('./events.js'), C = require('./constants.js');
 
 /**
- * @class anm.PlayerManager
+ * @singleton @class anm.PlayerManager
+ *
+ * Manages all the player instances on a page.
+ *
+ * To subscribe to a new player creation event, use:
+ *
+ * `anm.PlayerManager.on(C.S_NEW_PLAYER, function(player) { ... })`
+ *
+ * To subsribe to a player removal event, use:
+ *
+ * `anm.PlayerManager.on(C.S_PLAYER_DETACH, function(player) { ... })`
  */
 function PlayerManager() {
     this.hash = {};
@@ -20,6 +30,14 @@ PlayerManager.prototype.handle__x = function(evt, player) {
     }
     return true;
 }
+/**
+ * @method getPlayer
+ *
+ * Find a player on a page using an ID of its wrapper.
+ *
+ * @param {String} id ID of a player to find
+ * @return {anm.Player}
+ */
 PlayerManager.prototype.getPlayer = function(cvs_id) {
     return this.hash[cvs_id];
 }
