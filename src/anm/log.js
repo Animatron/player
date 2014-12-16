8dcab9591c4183a564b8b6ef38a6c2d6940bbd41
Yuriy Opryshko <mcm6987@gmail.com>
2014-12-16 23:36:37 +0300

    some more log fixes as window.console was somehow overwritten

--- src/anm/log.js
+++ src/anm/log.js
@@ -3,9 +3,9 @@ var conf = require('./conf.js'),
 
 var nop = function() {};
 var c = global.console || {log: nop, info: nop, warn: nop, error: nop},
-    сonsole;
+    anmConsole;
 if (global.console) {
-    console = {
+    anmConsole = {
         log: c.debug || c.log,
         info: c.info || c.log,
         warn: c.warn || c.log,
@@ -14,18 +14,18 @@ if (global.console) {
     if (!c.log.apply) {
         //in ie9, console.log isn't a real Function object and does not have .apply()
         //we will have to remedy this using Function.prototype.bind()
-        console.log = Function.prototype.bind.call(console.log, c);
-        console.info = Function.prototype.bind.call(console.info, c);
-        console.warn = Function.prototype.bind.call(console.warn, c);
-        console.error = Function.prototype.bind.call(console.log, c);
+        anmConsole.log = Function.prototype.bind.call(anmConsole.log, c);
+        anmConsole.info = Function.prototype.bind.call(anmConsole.info, c);
+        anmConsole.warn = Function.prototype.bind.call(anmConsole.warn, c);
+        anmConsole.error = Function.prototype.bind.call(anmConsole.log, c);
     }
 }
 
 var log = {
-    debug: function() { if (conf.logLevel & C.L_DEBUG) console.log.apply(c, arguments); },
-    info:  function() { if (conf.logLevel & C.L_INFO)  console.info.apply(c, arguments); },
-    warn:  function() { if (conf.logLevel & C.L_WARN)  console.warn.apply(c, arguments); },
-    error: function() { if (conf.logLevel & C.L_ERROR) console.error.apply(c, arguments); }
+    debug: function() { if (conf.logLevel & C.L_DEBUG) anmConsole.log.apply(c, arguments); },
+    info:  function() { if (conf.logLevel & C.L_INFO)  anmConsole.info.apply(c, arguments); },
+    warn:  function() { if (conf.logLevel & C.L_WARN)  anmConsole.warn.apply(c, arguments); },
+    error: function() { if (conf.logLevel & C.L_ERROR) anmConsole.error.apply(c, arguments); }
 };
 
 module.exports = log;
