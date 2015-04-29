74031fb2e25fb1dfcaec81becc8e0f5751eadbe4
Ulric Wilfred <shaman.sir@gmail.com>
2015-04-29 16:54:23 +0200

    sandbox is back!

---
+++ sandbox/examples.js
@@ -0,0 +1,261 @@
+var defaultCode = [
+  '// feel free to change,',
+  '// just leave `return` on its place',
+  '',
+  'return element()',
+  '    .add(',
+  '        element(\'blue-rect\').rect(70)',
+  '                            .move(140, 25)',
+  '                            .fill(\'#009\')',
+  '                            .stroke(\'#f00\', 3)',
+  '                            .modify(Tween.rotate().band(0, 10).values(0, Math.PI / 2)))',
+  '    .add(',
+  '        element(\'red-rect\').rect(60)',
+  '                           .move(115, 90)',
+  '                           .fill(\'#f00\'))',
+  '    .modify(Tween.rotate().band(0, 10).from(0).to(Math.PI));'
+].join('\n');
+
+var examples = [];
+examples.push([ 0 /*version*/, defaultCode ]);
+
+examples.push([ 0 /*version*/, [
+  '//                  x,   y,   r',
+  '',
+  'var circles = [ [  20,  20,  50 ],',
+  '                [  70,  30,  70 ],',
+  '                [  60, 120,  14 ],',
+  '                [ 140, 110,  20 ],',
+  '                [ 160, 160, 200 ] ];',
+  '',
+  'var o = element();',
+  '',
+  'for (var i = 0, clen = circles.length; i < clen; i++) {',
+  '    var cx = circles[i][0],',
+  '        cy = circles[i][1],',
+  '        cr = circles[i][2];',
+  '    o.add(element().move(cx, cy)',
+  '                   .oval(cr)',
+  '                   .stroke(\'#333\', 2).fill(\'#366\')',
+  '                   .modify(Tween.alpha().band(1.3, 3).values(1, .4))',
+  '                   .modify(function(t, duration) {',
+  '                       this.sx = 1 / t;',
+  '                       this.sy = 1 / t;',
+  '                    }));',
+  '}',
+  '',
+  'return o.modify(Tween.rotate().band(0, 3).from(Math.PI / 4).to(-(0.15 * Math.PI)));'
+].join('\n') ]);
+
+examples.push([ 0, [
+  'return elm().rect([50, 50], [40, 40])',
+  '            .trans([0, 3],',
+  '                   [[0, 0], [0, 150]],',
+  '                   C.E_COUT);'
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  'return elm()',
+  '    .add(',
+  '        elm().path([0, 0], \'M0 0 L40 40 C10 150 50 70 6 40 Z\')',
+  '             .stroke(\'#336\', 3)',
+  '             .fill(\'#674\')',
+  '             .modify(function() {',
+  '                 this.x = 150;',
+  '                 this.y = 150;',
+  '             })',
+  '             .rotate([0, 3], [0, Math.PI * 4]))',
+  '    .add(',
+  '        elm().paint(function(ctx) {',
+  '            ctx.lineWidth = 2;',
+  '            ctx.strokeStyle = \'#f35\';',
+  '            ctx.font = \'30pt serif\';',
+  '            ctx.strokeText(\'Boo!\', 50, 50);',
+  '        }))',
+  '/*.rotate([0, 3], [0, Math.PI])*/;',
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  '// See API Documentation (link is below)',
+  'var anim = new anm.Animation();',
+  'var elm = new anm.Element();',
+  'elm.$path = new anm.Path(\'M36 35 L35 70 L90 70 L50 20 L36 35 Z\',',
+  '                         { color: \'#f00\' },',
+  '                         { width: 2, color: \'#300\' });',
+  'elm.tween({',
+  '    type: C.T_ROTATE,',
+  '    band: [0, 3],',
+  '    data: [Math.PI / 6, 0]',
+  '});',
+  'elm.tween({',
+  '    type: C.T_TRANSLATE,',
+  '    band: [0, 3],',
+  '    data: anm.Path.parse(\'M-100 -100 L100 100 Z\')',
+  '});',
+  'elm.tween({',
+  '    type: C.T_ALPHA,',
+  '    band: [1.5, 3],',
+  '    data: [1, 0]',
+  '});',
+  'anim.add(elm);',
+  'return anim;'
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  'return elm()',
+  '    .add(elm().path([0, 0], \'M050 0 L20 20 C60 110 90 140 160 120 Z\'))',
+  '    .add(elm().rect([115, 90], [60, 60]))',
+  '    .rotate([0, 3], [-(Math.PI / 2), Math.PI / 2]);'
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  'return elm()',
+  '    .add(',
+  '        elm(\'blue-rect\').rect([100, 25], [70, 70])',
+  '                      .fill(\'#009\')',
+  '                      .stroke(\'#f00\', 3)',
+  '                      .alpha([0, 3], [0, 1])',
+  '                      .trans([0, 4], [[0, 0], [ 100, 100 ]])',
+  '                      .trans([4, 8], [[100, 100], [-200, 300]])',
+  '                      .scale([0, 10], [[1, 1], [.5, .5]]))',
+  '    .add(',
+  '        elm(\'def-rect\').rect([115, 90], [60, 60]));'
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  'return elm().image([90, 120], \'./res/bender.jpg\')',
+  '            .rotate([0, 3], [0, Math.PI / 4])',
+  '            .trans([0, 3], [[200, 50], [0, 50]])',
+  '            .xscale([0, 3], [.3, .1]);'
+].join('\n') ]);
+
+examples.push([ 0 /*version*/, [
+  'return elm().sprite([1, 1], \'./res/sprite_sample.png\', [144, 59])',
+  '            .pvt([0, 0])',
+  '            .animate(0, [0, 30], 10)'
+].join('\n') ]);
+
+/*
+return b('scene')
+  //.add(b('rect').rect([0, 0], 90).fill('rgba(255,255,0,.4)'))
+  //.add(b('rect').rect([0, 0], 90).fill('rgba(255,255,0,.4)').pvt([-.5, -.5]))
+  //.add(b('circle').circle([0, 0], 45).fill('rgba(90,0,90,.4)').pvt([.5, .5]))
+  //.add(b('circle').circle([0, 0], 145).fill('rgba(90,0,90,.4)').pvt([0, 0]))
+  //.add(b('path1').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([1, 1]))
+  //.add(b('path2').npath([110, 110], 'M50 50 L90 90 L50 90 L50 50 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([1, 1]))
+  //.add(b('path3').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([0, .5]))
+  //.add(b('image').image([0, 0], './res/sprite.png'))
+  //.add(b('text').text([0, 0], './res/sprite.png'))
+  //.add(b('sprite').sprite([1, 1], './res/sprite_sample.png', [144, 59]).pvt([0, 0])
+  //        .animate(0, [0, 30], 10).opacity(.5))
+;
+*/
+
+/*
+
+//.add(b('circle').circle([90, 90], 45).fill('rgba(90,0,90,.4)').pvt([1, 1]))
+  //.add(b('circle').circle([0, 0], 145).fill('rgba(90,0,90,.4)').pvt([.5, .5]))
+  //.add(b('path2').npath([110, 110], 'M50 50 L90 90 L50 90 L50 50 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([.5, .5]))
+  //.add(b('path1').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([.5, .5]))
+
+  //.add(b('path3').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
+  //                                                 .stroke('#000', 5).pvt([0, .5]))
+  //.add(b('image').image([0, 0], './res/sprite.png').pvt([0, 0]))
+  //.add(b('text').text([0, 0], './res/sprite.png').pvt([0, 0]))
+  .add(b('sprite').sprite([71, 71], './res/sprite_sample.png', [144, 59]).pvt([0, 0])
+          .animate(0, [0, 30], 10).opacity(.5))
+
+*/
+
+/* // RELOAD at 18 sec
+
+var test_scene =
+       b().add(test_for({ duration: 18,
+                          clip_band: [0, 15], clip_mode: C.R_ONCE,
+                          layer_1_band: [ 3, Infinity ], layer_1_mode: C.R_ONCE,
+                          layer_2_band: [ 2, Infinity ], layer_2_mode: C.R_ONCE,
+                          layer_3_band: [ 0, Infinity ], layer_3_mode: C.R_ONCE,
+                          layer_4_band: [ 0, 10 ],       layer_4_mode: C.R_ONCE,
+                          layer_5_band: [ 0, 10 ],       layer_5_mode: C.R_ONCE }))
+
+function test_for(conf) {
+    return b('wrapper').add(
+   b('scene').band([0, conf.duration ]).zoom(1.2)
+             .add(b('flash').band([0,.5]).rect([100, 100], 1000)
+                            .fill('#000').alpha([0,.5], [1, 0]))
+             .add(b('time').band([0,conf.duration]).paint(function(ctx) {
+                               ctx.save();
+                               ctx.font="14px sans-serif";
+                               ctx.fillText(Math.floor(player.state.time * 100) / 100, 230, 200);
+                               ctx.restore();
+                            }).fill('#000'))
+             .add(b('clip').band(conf.clip_band).mode(conf.clip_mode).move([15, 90])
+                           .add(b('layer1').rect([0, 0], 15).fill('#f6f600')
+                                           .band(conf.layer_1_band)
+                                           .mode(conf.layer_1_mode)
+                                           .trans([0, 5],[[0, 0],[40,0]])
+                                           .rotate([0, 5], [0, Math.PI / 4]))
+                           .add(b('layer2').rect([0, 20], 15).fill('#f60000')
+                                           .band(conf.layer_2_band)
+                                           .mode(conf.layer_2_mode))
+                           .add(b('layer3').rect([0, 40], 15).fill('#00f6f6')
+                                           .band(conf.layer_3_band)
+                                           .mode(conf.layer_3_mode)
+                                           .trans([0, 5],[[0, 0],[40,0]])
+                                           .rotate([0, 5], [0, Math.PI / 4]))
+                           .add(b('layer4').rect([0, 60], 15).fill('#00f600')
+                                           .band(conf.layer_4_band)
+                                           .mode(conf.layer_4_mode)
+                                           .trans([0, 5],[[0, 0],[40,0]])
+                                           .rotate([0, 5], [0, Math.PI / 4]))
+                           .add(b('layer5').rect([0, 80], 15).fill('#f600f6')
+                                           .band(conf.layer_5_band)
+                                           .mode(conf.layer_5_mode)
+                                           .trans([0, 10],[[0, 0],[40,0]])
+                                           .rotate([0, 10], [0, Math.PI / 4])))
+             .add(b('ghst').band([0, conf.clip_band[1]]).move([15, 90]).opacity(.2)
+                         //.alpha([conf.clip_band[1], conf.clip_band[1] + .5], [1, 0])
+                           .add(b('ghost1').rect([0, 0], 15).stroke('#f6f600').nofill())
+                           .add(b('ghost2').rect([0, 20], 15).stroke('#f60000').nofill())
+                           .add(b('ghost3').rect([0, 40], 15).stroke('#00f6f6').nofill())
+                           .add(b('ghost4').rect([0, 60], 15).stroke('#00f600').nofill())
+                           .add(b('ghost5').rect([0, 80], 15).stroke('#f600f6').nofill()))
+             .add(b('info').move([5, 3]).add(text([0, 0], 'duration: ' + conf.duration))
+                           .add(text([0, 10], 'clip band & mode: ' + conf.clip_band
+                                                  + ' — ' + mode_str(conf.clip_mode)))
+                           .add(text([0, 20], 'layer1 band & mode: ' + conf.layer_1_band
+                                                    + ' — ' + mode_str(conf.layer_1_mode))
+                                              .fill('#969600'))
+                           .add(text([0, 30], 'layer2 band & mode: ' + conf.layer_2_band
+                                                    + ' — ' + mode_str(conf.layer_2_mode))
+                                              .fill('#960000'))
+                           .add(text([0, 40], 'layer3 band & mode: ' + conf.layer_3_band
+                                                    + ' — ' + mode_str(conf.layer_3_mode))
+                                              .fill('#009696'))
+                           .add(text([0, 50], 'layer4 band & mode: ' + conf.layer_4_band
+                                                    + ' — ' + mode_str(conf.layer_4_mode))
+                                              .fill('#009600'))
+                           .add(text([0, 60], 'layer5 band & mode: ' + conf.layer_5_band
+                                                     + ' — ' + mode_str(conf.layer_5_mode))
+                                              .fill('#960096'))));
+}
+
+function mode_str(from) {
+    if (!from) return 'once';
+    if (from === C.R_ONCE) return "once";
+    if (from === C.R_STAY) return "stay";
+    if (from === C.R_LOOP) return "loop";
+    if (from === C.R_BOUNCE) return "bounce";
+}
+
+function text(pos, lines) {
+    return b().text(pos, lines, 10).pvt([0, 0]);
+}
+
+
+return test_scene; */
---
+++ sandbox/sandbox.js
@@ -0,0 +1,304 @@
+/*
+ * Copyright (c) 2011-2014 by Animatron.
+ * All rights are reserved.
+ *
+ * Animatron player is licensed under the MIT License, see LICENSE.
+ */
+
+var DEFAULT_REFRESH_RATE = 3000;
+
+var _player = null;
+
+function sandbox() {
+
+    this.codeElm = document.getElementById('animation-source');
+    this.errorsElm = document.getElementById('errors');
+    this.selectElm = document.getElementById('examples-list');
+    this.tangleElm = document.getElementById('refresh-calc');
+    this.debugElm = document.getElementById('enable-debug');
+    this.logErrorsElm = document.getElementById('log-errors');
+
+    var width = 420, height = 250;
+
+    var element = anm.Element._$;
+    var Tween = anm.Tween, tween = anm.Tween._$;
+
+    window.element = anm.Element._$;
+    window.Tween = anm.Tween;
+    window.tween = anm.Tween._$;
+
+    this.player = anm.createPlayer('player', {
+        muteErrors: true,
+        controlsEnabled: false,
+        width: width,
+        height: height
+    });
+
+    //this.player.mode = anm.C.M_SANDBOX;
+    //this.player._updateMode();
+
+    _player = this.player;
+
+    var lastCode = '';
+    if (localStorage) lastCode = load_last_code();
+
+    var logErrors = false;
+
+    this.cm = CodeMirror.fromTextArea(this.codeElm,
+              { mode: 'javascript',
+                indentUnit: 4,
+                lineNumbers: true,
+                //gutters: ['cm-margin-gutter'],
+                matchBrackets: true,
+                wrapLines: true/*,
+                autofocus: true*/ });
+    this.cm.setValue((lastCode.length > 0) ? lastCode : defaultCode);
+    //this.cm.setValue('return <your code here>;');
+    this.cm.setSize(null, '66%');
+    this.cm.on('focus', function() {
+        document.body.className = 'blur';
+    });
+    this.cm.on('blur', function() {
+        document.body.className = '';
+    });
+    this.cm.on('change', function() {
+        wereErrors = false;
+        refreshFromCurrentMoment();
+    });
+
+    var s = this;
+
+    var curTimeouts = [],
+        refreshRate = localStorage ? load_refresh_rate() : DEFAULT_REFRESH_RATE;
+
+    var lastPlay = -1,
+        lastSequence = -1,
+        wereErrors = false;
+
+    function makeSafe(code) {
+        return ['(function(){',
+                '  '+code,
+                '})();'].join('\n');
+    }
+
+    function playFrom(from, rate) {
+        s.errorsElm.style.display = 'none';
+        try {
+            _player.stop();
+            var userCode = s.cm.getValue();
+            if (localStorage) save_current_code(userCode);
+            var safeCode = makeSafe(userCode);
+            var anim = eval(safeCode);
+            if (!anim || (!(anim instanceof anm.Animation) && !(anim instanceof anm.Element))) {
+                throw new Error('No animation was returned from code');
+            }
+            _player.load(anim, rate / 1000);
+            _player.play(from / 1000);
+            lastPlay = Date.now() - from;
+        } catch(e) {
+            onerror(e);
+        }
+    };
+
+    function onerror(e) {
+        cleanCanvas();
+        ensureToCancelTimeouts();
+        wereErrors = true;
+        var e2;
+        try {
+          _player.anim = null;
+          _player.stop();
+          _player._drawErrorSplash(e);
+        } catch(e) { e2 = e; };
+        s.errorsElm.style.display = 'block';
+        s.errorsElm.innerHTML = '<strong>Error:&nbsp;</strong>'+e.message;
+        if (logErrors && console && console.error) {
+          console.error(e.stack);
+          if (e2) console.error(e2.stack);
+        }
+        //throw e;
+    }
+
+    this.player.onerror(onerror);
+
+    function ensureToCancelTimeouts() {
+        var count = curTimeouts.length;
+        //console.log('removing all of the timeouts (' + count + ')');
+        while (count) {
+            var timeout = curTimeouts[--count];
+            //console.log('cancelling timeout #' + timeout[0] + ' (' + timeout[1] + ')');
+            clearTimeout(timeout[0]);
+        }
+        curTimeouts = [];
+    }
+
+    function cleanCanvas() {
+        var cvs = document.getElementById('player-cvs');
+        if (cvs) cvs.getContext('2d').clearRect(0, 0, cvs.width, cvs.height );
+    }
+
+    function refreshFromStart() {
+        //console.log('refresh from start');
+        runSequence(refreshRate, 0);
+    }
+
+    function refreshFromCurrentMoment() {
+        //console.log('refresh from current moment');
+        runSequence(refreshRate, (Date.now() - lastPlay) % refreshRate);
+    }
+
+    function runSequence(rate, startAt) {
+        var sequenceId = ++lastSequence,
+            timeoutId = 0;
+        //console.log('runSequence #' + sequenceId);
+        ensureToCancelTimeouts();
+        refreshRate = rate;
+        save_refresh_rate(rate);
+        var _refresher = function(from, seqId, tId) {
+            return function(once) {
+                if (wereErrors) return;
+                //console.log('starting to play from ', from, ', refresher id is ',
+                //             seqId + '-' + tId, new Date());
+                playFrom(from, rate);
+                if (once) return;
+                var nextTimeout = seqId + '-' + (tId + 1);
+                curTimeouts.push([ setTimeout(_refresher(0, seqId, tId + 1), rate - from), nextTimeout ]);
+                //console.log('sheduled next refresh to ', rate - from, 'ms (#', nextTimeout, ')');
+            }
+        };
+        _refresher(startAt || 0, sequenceId, timeoutId)
+                  ( /* play once or not */
+                    (_player.handleEvents === true) ||
+                    (_player.infiniteDuration === true));
+    }
+
+    if (localStorage) {
+        setTimeout(function() {
+            store_examples(); // store current examples, it will skip if their versions match
+            load_examples(); // load new examples, it will skip the ones with matching versions
+            list_examples(s.selectElm); // list the examples in select element
+        }, 1);
+    }
+
+    this.selectElm.onchange = function() {
+        s.cm.setValue(examples[this.selectedIndex][1]);
+        wereErrors = false;
+        refreshFromStart();
+    }
+
+    this.debugElm.onchange = function() {
+        s.player.debug = !s.player.debug;
+        refreshFromCurrentMoment();
+    }
+
+    this.logErrorsElm.onchange = function() {
+        logErrors = !logErrors;
+    }
+
+    var tangleModel = {
+        initialize: function () {
+            this.secPeriod = refreshRate / 1000;
+        },
+        update: function () {
+            this.perMinute = Math.floor((60 / this.secPeriod) * 100) / 100;
+            runSequence(this.secPeriod * 1000, 0);
+        }
+    };
+
+    runSequence(refreshRate, 0);
+
+    new Tangle(this.tangleElm, tangleModel);
+
+    function change_mode(radio) {
+      if (_player) {
+        _player.handleEvents = (radio.value === 'with-events');
+        _player.infiniteDuration = (radio.value === 'with-events');
+        _player._checkOpts();
+        refreshFromStart();
+      }
+    }
+
+    window.change_mode = change_mode;
+
+}
+
+function store_examples() {
+    if (!localStorage) throw new Error('Local storage support required');
+    var elen = examples.length;
+    for (var i = 0; i < elen; i++) {
+        store_example(i);
+    }
+}
+
+function store_example(i) {
+    var ekey = '_example'+i,
+        vkey = ekey+'__v',
+        ver = localStorage.getItem(vkey);
+    if ((typeof ver === 'undefined') ||
+        (ver === null) ||
+        (ver < examples[i][0])) {
+        localStorage.setItem(vkey, examples[i][0]);
+        localStorage.setItem(ekey, examples[i][1]);
+    }
+    localStorage.setItem('_examples_count', examples.length);
+}
+
+function load_examples() {
+    if (!localStorage) throw new Error('Local storage support required');
+    var ckey = '_examples_count',
+        count = localStorage.getItem(ckey) || 0,
+        elen = examples.length;
+    for (var i = 0; i < count; i++) {
+        var ekey = '_example'+i,
+            vkey = ekey+'__v',
+            ver = localStorage.getItem(vkey);
+        if ((typeof ver !== 'undefined') &&
+            (ver !== null) &&
+            ((i >= elen) ||
+             (ver > examples[i][0]))) {
+            examples[i] = [
+                ver,
+                localStorage.getItem(ekey)
+            ];
+        }
+    }
+}
+
+function save_example(code) {
+    var pos = examples.length;
+    examples[pos] = [ 0, code ];
+    store_example(pos);
+}
+
+function save_current_code(code) {
+    if (!localStorage) throw new Error('Local storage support required');
+    localStorage.setItem('_current_code', code);
+}
+
+function load_last_code() {
+    if (!localStorage) throw new Error('Local storage support required');
+    return localStorage.getItem('_current_code') || '';
+}
+
+function save_refresh_rate(rate) {
+    if (!localStorage) throw new Error('Local storage support required');
+    localStorage.setItem('_current_rate', rate);
+}
+
+function load_refresh_rate() {
+    if (!localStorage) throw new Error('Local storage support required');
+    return localStorage.getItem('_current_rate') || DEFAULT_REFRESH_RATE;
+}
+
+function list_examples(selectElm) {
+    selectElm.innerHTML = '';
+    var elen = examples.length;
+    //selectElm.setAttribute('size', elen);
+    for (var i = 0; i < elen; i++) {
+        var optElm = document.createElement('option');
+        optElm.setAttribute('value', i);
+        optElm.innerHTML = i + ': [v' + examples[i][0] + '] : ' +
+                           examples[i][1].substring(0, 45).split('\n').join('↵');
+        selectElm.appendChild(optElm);
+    }
+}
