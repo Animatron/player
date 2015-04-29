c0be74850bc183c9b885e850a9fa0f1df603b878
Ulric Wilfred <shaman.sir@gmail.com>
2015-04-29 18:14:34 +0200

    show example names instead of weird information

--- sandbox/examples.js
+++ sandbox/examples.js
@@ -17,9 +17,9 @@ var defaultCode = [
 ].join('\n');
 
 var examples = [];
-examples.push([ 0 /*version*/, defaultCode ]);
+examples.push([ 0 /*version*/, 'Rectangles' /* name */, defaultCode ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Circles' /* name */, [
   '//                  x,   y,   r',
   '',
   'var circles = [ [  20,  20,  50 ],',
@@ -47,14 +47,14 @@ examples.push([ 0 /*version*/, [
   'return o.modify(Tween.rotate().band(0, 3).from(Math.PI / 4).to(-(0.15 * Math.PI)));'
 ].join('\n') ]);
 
-examples.push([ 0, [
+examples.push([ 0 /*version*/, 'Easings' /* name */, [
   'return elm().rect([50, 50], [40, 40])',
   '            .trans([0, 3],',
   '                   [[0, 0], [0, 150]],',
   '                   C.E_COUT);'
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Paths' /* name */, [
   'return elm()',
   '    .add(',
   '        elm().path([0, 0], \'M0 0 L40 40 C10 150 50 70 6 40 Z\')',
@@ -75,7 +75,7 @@ examples.push([ 0 /*version*/, [
   '/*.rotate([0, 3], [0, Math.PI])*/;',
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Merging Tweens' /* name */,  [
   '// See API Documentation (link is below)',
   'var anim = new anm.Animation();',
   'var elm = new anm.Element();',
@@ -101,14 +101,14 @@ examples.push([ 0 /*version*/, [
   'return anim;'
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Another Paths Example' /* name */, [
   'return elm()',
   '    .add(elm().path([0, 0], \'M050 0 L20 20 C60 110 90 140 160 120 Z\'))',
   '    .add(elm().rect([115, 90], [60, 60]))',
   '    .rotate([0, 3], [-(Math.PI / 2), Math.PI / 2]);'
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Another Rects Example' /* name */, [
   'return elm()',
   '    .add(',
   '        elm(\'blue-rect\').rect([100, 25], [70, 70])',
@@ -122,140 +122,15 @@ examples.push([ 0 /*version*/, [
   '        elm(\'def-rect\').rect([115, 90], [60, 60]));'
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Image' /* name */, [
   'return elm().image([90, 120], \'./res/bender.jpg\')',
   '            .rotate([0, 3], [0, Math.PI / 4])',
   '            .trans([0, 3], [[200, 50], [0, 50]])',
   '            .xscale([0, 3], [.3, .1]);'
 ].join('\n') ]);
 
-examples.push([ 0 /*version*/, [
+examples.push([ 0 /*version*/, 'Sprite Sheets' /* name */, [
   'return elm().sprite([1, 1], \'./res/sprite_sample.png\', [144, 59])',
   '            .pvt([0, 0])',
   '            .animate(0, [0, 30], 10)'
 ].join('\n') ]);
-
-/*
-return b('scene')
-  //.add(b('rect').rect([0, 0], 90).fill('rgba(255,255,0,.4)'))
-  //.add(b('rect').rect([0, 0], 90).fill('rgba(255,255,0,.4)').pvt([-.5, -.5]))
-  //.add(b('circle').circle([0, 0], 45).fill('rgba(90,0,90,.4)').pvt([.5, .5]))
-  //.add(b('circle').circle([0, 0], 145).fill('rgba(90,0,90,.4)').pvt([0, 0]))
-  //.add(b('path1').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([1, 1]))
-  //.add(b('path2').npath([110, 110], 'M50 50 L90 90 L50 90 L50 50 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([1, 1]))
-  //.add(b('path3').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([0, .5]))
-  //.add(b('image').image([0, 0], './res/sprite.png'))
-  //.add(b('text').text([0, 0], './res/sprite.png'))
-  //.add(b('sprite').sprite([1, 1], './res/sprite_sample.png', [144, 59]).pvt([0, 0])
-  //        .animate(0, [0, 30], 10).opacity(.5))
-;
-*/
-
-/*
-
-//.add(b('circle').circle([90, 90], 45).fill('rgba(90,0,90,.4)').pvt([1, 1]))
-  //.add(b('circle').circle([0, 0], 145).fill('rgba(90,0,90,.4)').pvt([.5, .5]))
-  //.add(b('path2').npath([110, 110], 'M50 50 L90 90 L50 90 L50 50 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([.5, .5]))
-  //.add(b('path1').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([.5, .5]))
-
-  //.add(b('path3').path([110, 110], 'M0 0 L90 90 L0 90 L0 0 Z').fill('rgba(90,90,0,.4)')
-  //                                                 .stroke('#000', 5).pvt([0, .5]))
-  //.add(b('image').image([0, 0], './res/sprite.png').pvt([0, 0]))
-  //.add(b('text').text([0, 0], './res/sprite.png').pvt([0, 0]))
-  .add(b('sprite').sprite([71, 71], './res/sprite_sample.png', [144, 59]).pvt([0, 0])
-          .animate(0, [0, 30], 10).opacity(.5))
-
-*/
-
-/* // RELOAD at 18 sec
-
-var test_scene =
-       b().add(test_for({ duration: 18,
-                          clip_band: [0, 15], clip_mode: C.R_ONCE,
-                          layer_1_band: [ 3, Infinity ], layer_1_mode: C.R_ONCE,
-                          layer_2_band: [ 2, Infinity ], layer_2_mode: C.R_ONCE,
-                          layer_3_band: [ 0, Infinity ], layer_3_mode: C.R_ONCE,
-                          layer_4_band: [ 0, 10 ],       layer_4_mode: C.R_ONCE,
-                          layer_5_band: [ 0, 10 ],       layer_5_mode: C.R_ONCE }))
-
-function test_for(conf) {
-    return b('wrapper').add(
-   b('scene').band([0, conf.duration ]).zoom(1.2)
-             .add(b('flash').band([0,.5]).rect([100, 100], 1000)
-                            .fill('#000').alpha([0,.5], [1, 0]))
-             .add(b('time').band([0,conf.duration]).paint(function(ctx) {
-                               ctx.save();
-                               ctx.font="14px sans-serif";
-                               ctx.fillText(Math.floor(player.state.time * 100) / 100, 230, 200);
-                               ctx.restore();
-                            }).fill('#000'))
-             .add(b('clip').band(conf.clip_band).mode(conf.clip_mode).move([15, 90])
-                           .add(b('layer1').rect([0, 0], 15).fill('#f6f600')
-                                           .band(conf.layer_1_band)
-                                           .mode(conf.layer_1_mode)
-                                           .trans([0, 5],[[0, 0],[40,0]])
-                                           .rotate([0, 5], [0, Math.PI / 4]))
-                           .add(b('layer2').rect([0, 20], 15).fill('#f60000')
-                                           .band(conf.layer_2_band)
-                                           .mode(conf.layer_2_mode))
-                           .add(b('layer3').rect([0, 40], 15).fill('#00f6f6')
-                                           .band(conf.layer_3_band)
-                                           .mode(conf.layer_3_mode)
-                                           .trans([0, 5],[[0, 0],[40,0]])
-                                           .rotate([0, 5], [0, Math.PI / 4]))
-                           .add(b('layer4').rect([0, 60], 15).fill('#00f600')
-                                           .band(conf.layer_4_band)
-                                           .mode(conf.layer_4_mode)
-                                           .trans([0, 5],[[0, 0],[40,0]])
-                                           .rotate([0, 5], [0, Math.PI / 4]))
-                           .add(b('layer5').rect([0, 80], 15).fill('#f600f6')
-                                           .band(conf.layer_5_band)
-                                           .mode(conf.layer_5_mode)
-                                           .trans([0, 10],[[0, 0],[40,0]])
-                                           .rotate([0, 10], [0, Math.PI / 4])))
-             .add(b('ghst').band([0, conf.clip_band[1]]).move([15, 90]).opacity(.2)
-                         //.alpha([conf.clip_band[1], conf.clip_band[1] + .5], [1, 0])
-                           .add(b('ghost1').rect([0, 0], 15).stroke('#f6f600').nofill())
-                           .add(b('ghost2').rect([0, 20], 15).stroke('#f60000').nofill())
-                           .add(b('ghost3').rect([0, 40], 15).stroke('#00f6f6').nofill())
-                           .add(b('ghost4').rect([0, 60], 15).stroke('#00f600').nofill())
-                           .add(b('ghost5').rect([0, 80], 15).stroke('#f600f6').nofill()))
-             .add(b('info').move([5, 3]).add(text([0, 0], 'duration: ' + conf.duration))
-                           .add(text([0, 10], 'clip band & mode: ' + conf.clip_band
-                                                  + ' — ' + mode_str(conf.clip_mode)))
-                           .add(text([0, 20], 'layer1 band & mode: ' + conf.layer_1_band
-                                                    + ' — ' + mode_str(conf.layer_1_mode))
-                                              .fill('#969600'))
-                           .add(text([0, 30], 'layer2 band & mode: ' + conf.layer_2_band
-                                                    + ' — ' + mode_str(conf.layer_2_mode))
-                                              .fill('#960000'))
-                           .add(text([0, 40], 'layer3 band & mode: ' + conf.layer_3_band
-                                                    + ' — ' + mode_str(conf.layer_3_mode))
-                                              .fill('#009696'))
-                           .add(text([0, 50], 'layer4 band & mode: ' + conf.layer_4_band
-                                                    + ' — ' + mode_str(conf.layer_4_mode))
-                                              .fill('#009600'))
-                           .add(text([0, 60], 'layer5 band & mode: ' + conf.layer_5_band
-                                                     + ' — ' + mode_str(conf.layer_5_mode))
-                                              .fill('#960096'))));
-}
-
-function mode_str(from) {
-    if (!from) return 'once';
-    if (from === C.R_ONCE) return "once";
-    if (from === C.R_STAY) return "stay";
-    if (from === C.R_LOOP) return "loop";
-    if (from === C.R_BOUNCE) return "bounce";
-}
-
-function text(pos, lines) {
-    return b().text(pos, lines, 10).pvt([0, 0]);
-}
-
-
-return test_scene; */
