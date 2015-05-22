a0dc78d1372b7f2084f927c0735057b6e8f041b1
Yuriy Opryshko <mcm6987@gmail.com>
2015-05-22 15:17:24 +0300

    add more audio tests

--- spec/media/audio.spec.js
+++ spec/media/audio.spec.js
@@ -1,10 +1,13 @@
 
-var nop = function(){}, empty = {};
+var nop = function(){}, empty = {},
+    resMan = {
+        loadOrGet: nop
+    };
 
 var proxyquire = require('proxyquire').noCallThru();
 var stubs = {
     '../conf.js': empty,
-    '../log.js': empty,
+    '../log.js': {error: nop},
     '../utils.js': empty,
     '../constants.js': empty,
     'engine': {
@@ -20,7 +23,7 @@ var stubs = {
         },
         appendToBody: nop
     },
-    '../resource_manager.js': empty
+    '../resource_manager.js': resMan
 };
 
 var Audio = proxyquire('../../src/anm/media/audio.js', stubs);
@@ -42,5 +45,39 @@ describe('Audio', function(){
         expect(audio.muted).toBeTruthy();
         audio.toggleMute();
         expect(audio.muted).toBeFalsy();
+    });
+    it('should call loadOrGet when loading', function() {
+        resMan.loadOrGet = function(id, url, cb, complete, error) {
+            complete({});
+        };
+
+        spyOn(resMan, 'loadOrGet');
+
+        var audio = new Audio('');
+        audio.load({}, {});
+
+        expect(resMan.loadOrGet).toHaveBeenCalled();
+    });
+    it('should set ready flag when loaded', function() {
+        resMan.loadOrGet = function(id, url, cb, complete, error) {
+            complete({});
+        };
+
+        var audio = new Audio('');
+        audio.load({}, {});
+
+        expect(audio.ready).toBeTruthy();
+    });
+    it('should throw an exception if loading failed', function() {
+        resMan.loadOrGet = function(id, url, cb, complete, error) {
+            error({});
+        };
+
+        var audio = new Audio('');
+        var load = function() {
+            audio.load({}, {});
+        };
+
+        expect(load).toThrow();
     });
 });
