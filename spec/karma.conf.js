474e5a5353d900a61fc77db2921de344341cfd65
Ulric Wilfred <shaman.sir@gmail.com>
2015-07-09 13:38:08 -0400

    add mocha reporter to dev distribution

--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -38,8 +38,7 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     //reporters: ['nyan'],
-    //reporters: ['mocha'],
-    reporters: ['dots'],
+    reporters: ['mocha'],
 
 
     // web server port
