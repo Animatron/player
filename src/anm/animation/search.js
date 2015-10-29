da9dbbf65bd6be58c70997964670f3411fcc4ad4
Ulric Wilfred <shaman.sir@gmail.com>
2015-10-29 18:28:01 +0100

    elements -> named in search, so scenes could also be used

--- src/anm/animation/search.js
+++ src/anm/animation/search.js
@@ -4,45 +4,45 @@ function Search(spec) {
     this.anywhere = (spec.selector[0] !== '/'); // search on all levels with one token
     this.tokens = (spec.selector[0] === '/') ? spec.selector.split('/').slice(1) : [ spec.selector ];
 };
-Search.prototype.over = function(elements) {
-    if (!this.multiple &&  this.anywhere) return search_one_anywhere(elements, this.tokens[0]);
-    if ( this.multiple &&  this.anywhere) return search_all_anywhere([], elements, this.tokens[0]);
-    if (!this.multiple && !this.anywhere) return search_one_by_path(elements, this.tokens);
-    if ( this.multiple && !this.anywhere) return search_all_by_path([], elements, this.tokens);
+Search.prototype.over = function(named) {
+    if (!this.multiple &&  this.anywhere) return search_one_anywhere(named, this.tokens[0]);
+    if ( this.multiple &&  this.anywhere) return search_all_anywhere([], named, this.tokens[0]);
+    if (!this.multiple && !this.anywhere) return search_one_by_path(named, this.tokens);
+    if ( this.multiple && !this.anywhere) return search_all_by_path([], named, this.tokens);
 };
 
-function matches(element, token, idx) {
+function matches(named, token, idx) {
     return (((token[0] === ':') && (idx === parseInt(token.slice(1)))) ||
-            (token === element.name));
+            (token === named.name));
 }
 
-function search_one_anywhere(elements, token) {
-    for (var i = 0; i < elements.length; i++) {
-        if (matches(elements[i], token, i)) return elements[i];
-        var result = search_one_anywhere(elements[i].children, token);
+function search_one_anywhere(named, token) {
+    for (var i = 0; i < named.length; i++) {
+        if (matches(named[i], token, i)) return named[i];
+        var result = search_one_anywhere(named[i].children, token);
         if (result) return result;
     }
     return null;
 }
 
-function search_all_anywhere(results, elements, token) {
-    for (var i = 0; i < elements.length; i++) {
-        if (matches(elements[i], token, i)) results.push(elements[i]);
-        search_all_anywhere(results, elements[i].children, token);
+function search_all_anywhere(results, named, token) {
+    for (var i = 0; i < named.length; i++) {
+        if (matches(named[i], token, i)) results.push(named[i]);
+        search_all_anywhere(results, named[i].children, token);
     }
     return results;
 }
 
-function search_one_by_path(elements, tokens, level) {
+function search_one_by_path(named, tokens, level) {
     level = level || 0;
     if ((tokens.length === 0) || (level >= tokens.length)) return null;
     var token = tokens[level];
     var lastLevel = (level === (tokens.length - 1));
-    for (var i = 0; i < elements.length; i++) {
-        if (matches(elements[i], token, i)) {
-            if (lastLevel) return elements[i];
+    for (var i = 0; i < named.length; i++) {
+        if (matches(named[i], token, i)) {
+            if (lastLevel) return named[i];
             else {
-                var result = search_one_by_path(elements[i].children, tokens, level + 1);
+                var result = search_one_by_path(named[i].children, tokens, level + 1);
                 if (result) return result;
             }
         }
@@ -50,15 +50,15 @@ function search_one_by_path(elements, tokens, level) {
     return null;
 }
 
-function search_all_by_path(results, elements, tokens, level) {
+function search_all_by_path(results, named, tokens, level) {
     level = level || 0;
     if ((tokens.length === 0) || (level >= tokens.length)) return results;
     var token = tokens[level];
     var lastLevel = (level === (tokens.length - 1));
-    for (var i = 0; i < elements.length; i++) {
-        if (matches(elements[i], token, i)) {
-            if (lastLevel) results.push(elements[i]);
-            else search_all_by_path(results, elements[i].children, tokens, level + 1);
+    for (var i = 0; i < named.length; i++) {
+        if (matches(named[i], token, i)) {
+            if (lastLevel) results.push(named[i]);
+            else search_all_by_path(results, named[i].children, tokens, level + 1);
         }
     }
     return results;
