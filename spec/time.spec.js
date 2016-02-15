9476f35621515c7154ab6e02a9e1a73808e67ee8
Ulric Wilfred <shaman.sir@gmail.com>
2016-02-15 17:10:12 +0100

    nicer schemes for time tests

--- spec/time.spec.js
+++ spec/time.spec.js
@@ -25,6 +25,10 @@ describe('time', function() {
         it('ends when default scene was finished', function() {
             var anim = new anm.Animation();
             anim.getCurrentScene().setDuration(5);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            //       [------------scene------------]
+
             var endSpy = jasmine.createSpy('end');
             anim.timeline.on(anm.C.X_END, endSpy);
             anim.tick(2);
@@ -38,12 +42,16 @@ describe('time', function() {
             var anim = new anm.Animation();
             anim.getCurrentScene().setDuration(5);
             anim.addScene('Bar', 10);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6....15]
+            //       [----------default------------][----Bar----]
+
             var endSpy = jasmine.createSpy('end');
             anim.timeline.on(anm.C.X_END, endSpy);
-            anim.tick(2);
-            anim.tick(10);
+            anim.tick(2);  // 2
+            anim.tick(10); // 12
             expect(endSpy).not.toHaveBeenCalled();
-            anim.tick(9);
+            anim.tick(9);  // 21
             expect(endSpy).toHaveBeenCalled();
             expect(anim.getTime()).toBe(anm.Timeline.NO_TIME);
         });
@@ -55,12 +63,49 @@ describe('time', function() {
             anim.replaceFirstScene('Foo', 10);
             anim.addScene('Bar', 10);
 
-            anim.tick(5);
+            // anim: 0-----1-----2-----3-----4....10----11....20]
+            //       [------------Foo--------------][----Bar----]
+
+            anim.tick(5.0); // 5
             expect(anim.getCurrentScene().name).toBe('Foo');
 
-            anim.tick(5.1);
+            anim.tick(5.1); // 10.1
             expect(anim.getCurrentScene().name).toBe('Bar');
 
+        });
+
+    });
+
+    describe('time bands', function() {
+
+        it('bands work properly for children', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            var child = new anm.Element();
+            anim.setDuration(10);
+            root.changeBand(1.0, 10);
+            child.changeBand(1.0, 10);
+            anim.add(root);
+            root.at(3.0, function() {
+                this.jump(1.0);
+            });
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root:       0-----1-----2-----3-----4-----5-
+            // chld:             0-----1-----2-----3-----4-
+
+            anim.tick(0.5); // 0.5
+            expect(root.getTime()).toBe(anm.Timeline.NO_TIME);
+            expect(child.getTime()).toBe(anm.Timeline.NO_TIME);
+            anim.tick(0.5); // 1.0
+            expect(root.getTime()).toBe(0.0);
+            expect(child.getTime()).toBe(anm.Timeline.NO_TIME);
+            anim.tick(1.0); // 2.0
+            expect(root.getTime()).toBe(1.0);
+            expect(root.getTime()).toBe(0.0);
+            anim.tick(1.0); // 3.0
+            expect(root.getTime()).toBe(2.0);
+            expect(root.getTime()).toBe(1.0);
         });
 
     });
@@ -73,10 +118,14 @@ describe('time', function() {
             anim.setDuration(10);
             anim.add(root);
             var actionSpy = jasmine.createSpy('action');
+
+            // anim: 0-----1-----2-----3-----
+            // root: 0--○--1-----2-----3-----
+
             root.at(0.5, actionSpy);
-            anim.tick(0.2);
+            anim.tick(0.2); // 0.2
             expect(actionSpy).not.toHaveBeenCalled();
-            anim.tick(0.4); // 0.2 + 0.4 == 0.6
+            anim.tick(0.4); // 0.6
             expect(actionSpy).toHaveBeenCalled();
         });
 
@@ -85,6 +134,10 @@ describe('time', function() {
             var root = new anm.Element();
             anim.setDuration(10);
             anim.add(root);
+
+            // anim: 0---●-1-----2-----3-----
+            // root: 0--○--1-----2-----3-----
+
             var actionSpy = jasmine.createSpy('action');
             root.at(0.5, actionSpy);
             anim.tick(0.6);
@@ -95,14 +148,19 @@ describe('time', function() {
             var anim = new anm.Animation();
             var root = new anm.Element();
             anim.setDuration(10);
-            root.changeBand(0.3, 5);
+            root.changeBand(1.2, 5);
             anim.add(root);
+
+            //                   2
+            // anim: 0-----1-----●-----3-----4-----5-----6-
+            // root:        [0--○--1-----2-----3---]
+
             var actionSpy = jasmine.createSpy('action');
-            root.at(0.5, actionSpy); // 0.3 + 0.5 == 0.8
-            anim.tick(0.6);
+            root.at(0.5, actionSpy); // 1.2 + 0.5 == 1.7
+            anim.tick(1.0); // 1.0
             expect(actionSpy).not.toHaveBeenCalled();
-            anim.tick(0.6); // 0.6 + 0.6 == 1.2
-            expect(actionSpy).toHaveBeenCalledWith(1.2 - 0.3);
+            anim.tick(1.0); // 2.0
+            expect(actionSpy).toHaveBeenCalledWith(2 - 1.7);
         });
 
         it('passes owner as `this` to the action', function() {
@@ -110,6 +168,10 @@ describe('time', function() {
             var root = new anm.Element();
             anim.setDuration(10);
             anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0--○--1-----2-----3-----4-----5-----6-
+
             var actionSpy = jasmine.createSpy('action');
             root.at(0.5, actionSpy.and.callFake(function() {
                 expect(this).toBe(root);
@@ -127,8 +189,14 @@ describe('time', function() {
             var root = new anm.Element();
             anim.setDuration(10);
             anim.add(root);
-            anim.tick(1.5);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1-----2-----3-----4-----5-----6-
+
+            anim.tick(1.5); // 1.5
             expect(root.getTime()).toBe(1.5);
+            anim.tick(1.0); // 2.5
+            expect(root.getTime()).toBe(2.5);
         });
 
         it('time pauses properly', function() {
@@ -136,8 +204,14 @@ describe('time', function() {
             var root = new anm.Element();
             anim.setDuration(10);
             anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1·······························
+
             root.timeline.pauseAt(1.0);
-            anim.tick(1.5);
+            anim.tick(1.5); // 1.5
+            expect(root.getTime()).toBe(1.0);
+            anim.tick(1.0); // 2.5
             expect(root.getTime()).toBe(1.0);
         });
 
@@ -146,11 +220,15 @@ describe('time', function() {
             var root = new anm.Element();
             anim.setDuration(10);
             anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1·····1-----2-----3-----4-----5-
+
             anim.at(1.0, function() { root.pause(); });
             anim.at(2.0, function() { root.continue(); });
-            anim.tick(1.5);
+            anim.tick(1.5); // 1.5
             expect(root.getTime()).toBe(1.0);
-            anim.tick(1.0);
+            anim.tick(1.0); // 2.5
             expect(root.getTime()).toBe(1.5);
         });
 
@@ -161,11 +239,38 @@ describe('time', function() {
             anim.setDuration(10);
             root.add(child);
             anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1·····1-----2-----3-----4-----5-
+            // chld: 0-----1·····1-----2-----3-----4-----5-
+
             anim.at(1.0, function() { root.pause(); });
             anim.at(2.0, function() { root.continue(); });
-            anim.tick(1.5);
+            anim.tick(1.5); // 1.5
             expect(child.getTime()).toBe(1.0);
-            anim.tick(1.0);
+            anim.tick(1.0); // 2.5
+            expect(child.getTime()).toBe(1.5);
+        });
+
+        it('`affectsChildren` (`true` by default) flag stops time both in the element and it\'s children, with bands', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            var child = new anm.Element();
+            anim.setDuration(10);
+            root.add(child);
+            anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root:       0-----1·····1-----2-----3-----4-
+            // chld:             0·····0-----1-----2-----3-
+
+            anim.at(2.0, function() { root.pause(); });
+            anim.at(3.0, function() { root.continue(); });
+            anim.tick(1.5); // 1.5
+            expect(child.getTime()).toBe(anm.Timeline.NO_TIME);
+            anim.tick(1.0); // 2.5
+            expect(child.getTime()).toBe(0);
+            anim.tick(2.0); // 4.5
             expect(child.getTime()).toBe(1.5);
         });
 
@@ -177,12 +282,40 @@ describe('time', function() {
             anim.setDuration(10);
             root.add(child);
             anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1·····1-----2-----3-----4-----5-
+            // chld: 0-----1-----2-----3-----4-----5-----6-
+
             anim.at(1.0, function() { root.pause(); });
             anim.at(2.0, function() { root.continue(); });
-            anim.tick(1.5);
+            anim.tick(1.5); // 1.5
             expect(child.getTime()).toBe(1.5);
-            anim.tick(1.0);
+            anim.tick(1.0); // 2.5
             expect(child.getTime()).toBe(2.5);
+        });
+
+        it('when `affectsChildren` is `false`, it stops time in the element, but not in it\'s children, relatively to parent band', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            root.affectsChildren = false;
+            var child = new anm.Element();
+            anim.setDuration(10);
+            root.changeBand(1.0, 10);
+            child.changeBand(1.0, 10);
+            root.add(child);
+            anim.add(root);
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root:       0-----1·····1-----2-----3-----4-
+            // chld:             0-----1-----2-----3-----4-
+
+            anim.at(2.0, function() { root.pause(); });
+            anim.at(3.0, function() { root.continue(); });
+            anim.tick(1.5); // 1.5
+            expect(child.getTime()).toBe(anm.Timeline.NO_TIME);
+            anim.tick(1.0); // 2.5
+            expect(child.getTime()).toBe(0.5);
         });
 
         // TODO: with repeat modes, several pauses, ...
@@ -201,11 +334,14 @@ describe('time', function() {
                 this.jump(3.0);
             });
 
-            anim.tick(1.0);
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1----2>3----4-----5-----6-----7-
+
+            anim.tick(1.0); // 1.0
             expect(root.getTime()).toBe(1.0);
-            anim.tick(1.0); // jump performed here
+            anim.tick(1.0); // 2.0
             expect(root.getTime()).toBe(3.0);
-            anim.tick(2.0);
+            anim.tick(2.0); // 4.0
             expect(root.getTime()).toBe(5.0);
 
         });
@@ -220,11 +356,14 @@ describe('time', function() {
                 this.jump(1.0);
             });
 
-            anim.tick(1.0); // anim.getTime() == 1.0
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1----2>1----2-----3-----4-----5-
+
+            anim.tick(1.0); // 1.0
             expect(root.getTime()).toBe(1.0);
-            anim.tick(1.0); // anim.getTime() == 2.0 // jump performed here
+            anim.tick(1.0); // 2.0 // jump performed here
             expect(root.getTime()).toBe(1.0);
-            anim.tick(2.0); // anim.getTime() == 4.0
+            anim.tick(2.0); // 4.0
             expect(root.getTime()).toBe(3.0);
 
         });
@@ -239,18 +378,87 @@ describe('time', function() {
                 this.jump(4.0);
             });
 
-            anim.tick(1.0);
-            expect(root.getTime()).toBe(0.0); // root band started
-            anim.tick(1.0);
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root:       0-----1----2>4----5-----6-----7-
+
+            anim.tick(1.0); // 1.0
+            expect(root.getTime()).toBe(0.0);
+            anim.tick(1.0); // 2.0
             expect(root.getTime()).toBe(1.0);
-            anim.tick(2.0); // jump performed here + 1.0sec
-            // animation time now: 4sec
-            // root jumped at animation time == 3sec (root's own 2sec)
-            // to its own 4sec, and then 1sec more passed after a jump
+            anim.tick(2.0); // 4.0
             expect(root.getTime()).toBe(5.0);
         });
 
-        // TODO: jumps with `affectsChildren` == `false`
+        it('jumps several times', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            anim.setDuration(10);
+            anim.add(root);
+            root.at(3.0, function() {
+                this.jump(1.0);
+            });
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-
+            // root: 0-----1-----2----3>1----2----3>1----2-
+
+            anim.tick(5.5); // jump should be performed twice
+            expect(root.getTime()).toBe(1.5);
+        });
+
+        it('also jumps twice in a child when jumps are done in parent', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            var child = new anm.Element();
+            anim.setDuration(10);
+            root.changeBand(1.0, 10);
+            child.changeBand(1.0, 10);
+            anim.add(root);
+            root.at(3.0, function() {
+                this.jump(1.0);
+            });
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-----7-
+            // root:       0-----1-----2----3>1----2----3>1----2-
+            // chld:             0-----1----2}0----1----2}0----1-
+
+            anim.tick(3.5); // 3.5
+            expect(root.getTime()).toBe(2.5);
+            expect(child.getTime()).toBe(1.5);
+            anim.tick(1.0); // 4.5
+            expect(root.getTime()).toBe(1.5);
+            expect(child.getTime()).toBe(0.5);
+            anim.tick(2.1); // 6.6
+            expect(root.getTime()).toBe(1.6);
+            expect(child.getTime()).toBe(0.6);
+        });
+
+        it('also jumps twice in a child when jumps are done in parent with `affectsChildren` == `false`', function() {
+            var anim = new anm.Animation();
+            var root = new anm.Element();
+            var child = new anm.Element();
+            anim.setDuration(10);
+            root.affectsChildren = false;
+            root.changeBand(1.0, 10);
+            child.changeBand(1.0, 10);
+            anim.add(root);
+            root.at(3.0, function() {
+                this.jump(1.0);
+            });
+
+            // anim: 0-----1-----2-----3-----4-----5-----6-----7-
+            // root:       0-----1-----2----3>1----2----3>1----2-
+            // chld:             0-----1-----2-----3-----4-----5-
+
+            anim.tick(3.5); // 3.5
+            expect(root.getTime()).toBe(2.5);
+            expect(child.getTime()).toBe(1.5);
+            anim.tick(1.0); // 4.5
+            expect(root.getTime()).toBe(1.5);
+            expect(child.getTime()).toBe(2.5);
+            anim.tick(2.1); // 6.6
+            expect(root.getTime()).toBe(1.6);
+            expect(child.getTime()).toBe(4.6);
+        });
 
         // TODO: with repeat modes, several jumps, ...
 
